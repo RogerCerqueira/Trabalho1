2a9b7e3d40b6d99f119e7b5ae8e4665aba8bc178
Roger Cerqueira <rogerdisgracahehe@gmail.com>
2026-05-15 13:31:26 -0300

    questao 4

---
+++ q4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char txt[250];
+    char palavra[50];
+
+    printf("Redija um texto:\n");
+    fgets(txt, 250, stdin);
+
+    printf("Informe a palavre:\n");
+    fgets(palavra, 50, stdin);
+
+    int tamPalavra = strelen(palavra);
+    int qtdeLetras = 0;
+
+    for(int i = 0; txt[i]; i++){
+        if(txt[i]==palavra[0]){
+            qtdeLetras = 1;
+            for (int j = 1; j < tamPalavra; j++){
+                if(txt[i+j] != palavra[j]){
+                    break;
+                }
+                qtdeLetras++;
+                
+                if(qtdeLetras == tamPalavra){
+                    
+                })
+            } 
+        }
+
+    }
+}
